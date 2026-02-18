de5df62abb288faa46bc74c3a1c64ad23db2dec3
dogunes1-sys <dogunes1@gmail.com>
2026-02-18 16:35:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from streamlit_gsheets import GSheetsConnection
 
 st.title("🎮 Ders RPG Kontrol Paneli")
 
-# En sade bağlantı şekli
+# Bağlantı
 conn = st.connection("gsheets", type=GSheetsConnection)
 
+# URL (Senin tablon)
 url = "https://docs.google.com/spreadsheets/d/1NJob3RNvMZ43_JlG1hnaZmnF_I3bUW3BtW9bsNx6kB8/edit?usp=sharing"
-data = conn.read(spreadsheet=url)
 
-st.dataframe(data)
+# Veriyi oku ve göster
+try:
+    data = conn.read(spreadsheet=url)
+    st.dataframe(data)
+except Exception as e:
+    st.error(f"Hata: {e}")
